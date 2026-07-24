efdaf27a95d262bf4ac1701d3fa9a27dbf257f78
Alejo, Ivan Judiel C. <260946099+IvanAlejo04@users.noreply.github.com>
2026-07-24 12:49:59 +0800

    v1.0.3
    remove the "NEW_DATA_FLAG"
    and made some changes

--- Top_Module.v
+++ Top_Module.v
@@ -1,3 +1,4 @@
+
 module PID_TOP (
 
     input PCLK,
@@ -9,11 +10,10 @@ module PID_TOP (
     input signed [32:0] Ki,
     input signed [32:0] Kd,
 
-    input signed [32:0] NEW_DATA_FLAG,
     input signed [32:0] MAX_CLMP,
     input signed [32:0] MIN_CLMP,
 
-    output signed [50:0] PID /* Turn to 50 based on problem I encounter where the output is not matching the 
+    output signed [49:0] PID /* Turn to 50 based on problem I encounter where the output is not matching the 
     intended output due to its size overflowing the bit range  */
 
 );
@@ -35,6 +35,8 @@ module PID_TOP (
   //======================//
   reg signed [32:0] past_error;
 
+  wire signed [49:0] PID_ANS = (P + I + D) >>> 16;
+
   proportion pterm (
       .KP (Kp),
       .ERR(ERR),
@@ -46,7 +48,7 @@ module PID_TOP (
       .ERR(ERR),
       .MAX_CLMP(MAX_CLMP),
       .MIN_CLMP(MIN_CLMP),
-      .accumulate(),  // mark
+      .accumulate(accumulate),  // mark
       .P(P),
       .D(D),
       .I(I)
@@ -57,12 +59,26 @@ module PID_TOP (
   derivative dterm (
       .KD(Kd),
       .ERR(ERR),
-      .PAST_ERR(),  // mark
+      .PAST_ERR(past_error),  // mark
       .D(D)
   );  
 
+ always @(posedge PCLK or posedge RST) begin
+        if (RST) begin
+            accumulate <= 32'b0;
+            past_error <= 32'b0;
+            PID <= 51'b0;
+        end else begin
 
-assign PID = (P + I + D) >>> ;// mark
+          if (PID_ANS >= MAX_CLMP || PID_ANS <= MIN_CLMP) begin
+              accumulate <= accumulate;  
+          end else begin
+              accumulate <= accumulate + ERR;
+          end
+            past_error <= ERR;                
+            PID <= PID_ANS;
+        end
+    end
 
 
 endmodule
